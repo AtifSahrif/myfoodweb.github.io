516016928b5b362b2af0094f47b943df7d7b1144
AtifSahrif <87377021+AtifSahrif@users.noreply.github.com>
2021-07-23 23:42:09 +0500

    Update App.js

--- js/App.js
+++ js/App.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $('.food-slider').slick({
-        // autoplay:true,
+         autoplay:true,
         slidesToShow: 3,
         // slidesToScroll:1,
         prevArrow:".prev-btn",
